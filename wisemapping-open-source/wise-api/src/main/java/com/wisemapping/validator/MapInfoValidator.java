4bb48875188a16488b52e8f56c9df50c16fb40af
gbLee <kblee@ispark.kr>
2026-02-24 17:08:05 +0900

    사용자 동일 마인드맵명 생성 제한 삭제

--- wisemapping-open-source/wise-api/src/main/java/com/wisemapping/validator/MapInfoValidator.java
+++ wisemapping-open-source/wise-api/src/main/java/com/wisemapping/validator/MapInfoValidator.java
@@ -20,7 +20,6 @@ package com.wisemapping.validator;
 
 import com.wisemapping.model.Constants;
 import com.wisemapping.model.Mindmap;
-import com.wisemapping.model.Account;
 import com.wisemapping.service.MindmapService;
 import com.wisemapping.view.MindMapInfoBean;
 import org.jetbrains.annotations.NotNull;
@@ -69,14 +68,6 @@ public class MapInfoValidator implements Validator {
                 errors.rejectValue("title", "field.max.length",
                         new Object[]{Constants.MAX_MAP_NAME_LENGTH},
                         "The title must have less than " + Constants.MAX_MAP_NAME_LENGTH + " characters.");
-            } else {
-                // Map already exists ?
-                final MindmapService service = this.getMindmapService();
-                final Account user = com.wisemapping.security.Utils.getUser();
-                final Mindmap mindMap = service.getMindmapByTitle(title, user);
-                if (mindMap != null) {
-                    errors.rejectValue("title", Messages.MAP_TITLE_ALREADY_EXISTS);
-                }
             }
         }
         ValidatorUtils.rejectIfExceeded(errors,
